f0eddd00cb231a794f786b8a7d2eacdade54e736
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-01-07 12:14:15 -0300

    Entrega Final

--- src/components/NavBar/NavBar desplegable.jsx
+++ src/components/NavBar/NavBar desplegable.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Select from "react-select";
+import { Link, useNavigate } from "react-router-dom";
 import CartWidget from "./CartWidget";
 import logo from "../../assets/icons/icon_pe.jpg";
 import "./NavBar.css";
 
 export default function NavBar() {
+	const dropdownMenuOptions = [
+		{ id: 1, label: "Estética", value: "/category/Estética" },
+		{ id: 2, label: "Prótesis", value: "/category/Prótesis" },
+		{ id: 3, label: "Rehabilitación", value: "/category/Rehabilitación" },
+	];
+
+	const navigate = useNavigate();
+
+	const handleSelectChange = (event) => {
+		navigate(event.value);
+	};
+
 	return (
 		<header id="header">
 			<nav className="bs--navbar">
@@ -19,19 +32,20 @@ export default function NavBar() {
 						</Link>
 					</div>
 					<div className="col">
+						<Select
+							className="dropdown--nav__link"
+							defaultValue={{
+								label: "Categorías:",
+								value: "empty",
+							}}
+							options={dropdownMenuOptions.map((sup) => ({
+								label: sup.label,
+								value: sup.value,
+							}))}
+							onChange={handleSelectChange}
+						></Select>
+
 						<ul className="nav__list">
-							<li>
-								<Link to="/category/Estética" className="nav__link">Estética</Link>
-							</li>
-							<li>
-								<Link to="/category/Prótesis" className="nav__link">Prótesis</Link>
-							</li>
-							<li>
-								<Link
-									to="/category/Rehabilitación"
-									className="nav__link"
-								>Rehabilitación</Link>
-							</li>
 							<li>
 								<Link to="/servicios" className="nav__link">
 									Servicios
